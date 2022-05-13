6ee5233d8b5af53451b7adcbe1d2a808e233de0d
alvarolobooooooooooooooooooo <105078434+alvarolobooooooooooooooooooo@users.noreply.github.com>
2022-05-13 13:58:55 -0600

    Creacion del menu

--- src/lab4p2_diego_alvaro/Lab4P2_Diego_Alvaro.java
+++ src/lab4p2_diego_alvaro/Lab4P2_Diego_Alvaro.java
@@ -15,7 +15,9 @@ public class Lab4P2_Diego_Alvaro {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("====================================");
+        System.out.println("            Menu                    ");
+        System.out.println("====================================");
     }
     
 }
