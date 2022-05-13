275ae31fce7d03c4ecabd0e363ed41780d4769ca
dcasco02 <dcasco@unitec.edu>
2022-05-13 13:59:29 -0600

    Clase Persona

--- src/lab4p2_diego_alvaro/Personas.java
+++ src/lab4p2_diego_alvaro/Personas.java
@@ -10,5 +10,56 @@ package lab4p2_diego_alvaro;
  * @author dcasc
  */
 public class Personas {
-    
+    private String nombre;
+    private int ataque;
+    private int vida;
+    private String nombreuniverso;
+
+    public Personas() {
+    }
+
+    public Personas(String nombre, int ataque, int vida, String nombreuniverso) {
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.vida = vida;
+        this.nombreuniverso = nombreuniverso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public String getNombreuniverso() {
+        return nombreuniverso;
+    }
+
+    public void setNombreuniverso(String nombreuniverso) {
+        this.nombreuniverso = nombreuniverso;
+    }
+
+    @Override
+    public String toString() {
+        return "Personas{" + "nombre=" + nombre + ", ataque=" + ataque + ", vida=" + vida + ", nombreuniverso=" + nombreuniverso + '}';
+    }
+            
 }
