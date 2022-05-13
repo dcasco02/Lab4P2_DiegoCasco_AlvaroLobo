3a78b69779d41d4579c31b791e2cd99704667a76
dcasco02 <dcasco@unitec.edu>
2022-05-13 15:06:41 -0600

    Enemigos

--- src/lab4p2_diego_alvaro/Enemigos.java
+++ src/lab4p2_diego_alvaro/Enemigos.java
@@ -9,6 +9,44 @@ package lab4p2_diego_alvaro;
  *
  * @author dcasc
  */
-public class Enemigos {
-    
+public class Enemigos extends Personas {
+  private String tipo;
+
+    public Enemigos() {
+    super();
+    }
+
+    public Enemigos(String tipo, String nombre, int ataque, int vida, String nombreuniverso) {
+        super(nombre, ataque, vida, nombreuniverso);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(float ataque) {
+        this.ataque = ataque;
+    }
+
+    @Override
+    public String toString() {
+        return "Enemigos{" + "tipo=" + tipo + '}';
+    }
+    public float ataque(){
+        if(tipo=="Blindado"){
+            return (float) (ataque*0.10);
+        }else if(tipo=="Alienigena"){
+            return (float)((float)ataque*0.30);
+        }
+        return ataque;
+    }
 }
